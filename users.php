bd84655dde13d66beb522d0b12158a015334f66d
Luke Taaffe <ltaaffe@names.co.uk>
2019-03-28 12:39:48 +0000

    Move data store config to separate files

---
+++ users.php
@@ -0,0 +1,29 @@
+<?php
+
+$userData = [
+    'John' => [
+        'name' => 'John',
+        'type' => 'beginner',
+        'workoutSet' => []
+    ],
+    'Lisa' => [
+        'name' => 'Lisa',
+        'type' => 'beginner',
+        'workoutSet' => []
+    ],
+    'Ronald' => [
+        'name' => 'Ronald',
+        'type' => 'advanced',
+        'workoutSet' => []
+    ],
+    'Steve' => [
+        'name' => 'Steve',
+        'type' => 'advanced',
+        'workoutSet' => []
+    ],
+    'Ron' => [
+        'name' => 'Ron',
+        'type' => 'advanced',
+        'workoutSet' => []
+    ],
+];
