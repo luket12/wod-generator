0259600b1c6e8cbeaae3b339786ce683a20eca03
Luke Taaffe <ltaaffe@names.co.uk>
2019-03-28 15:51:45 +0000

    Fix control structure

--- generator-oo.php
+++ generator-oo.php
@@ -23,19 +23,19 @@ $isConsole = (isset($argv) && count($argv) > 0);
 if (!$isConsole) {
     define('TOTALSETS', 30);
     define('SETINSECONDS', 60);
-}
+} else {
+    if (count($argv) < 3 || !is_int((int) $argv[1]) || !is_int((int) $argv[2])) {
+        exit("Please enter in the following format - generator {number of sets} {set time in seconds}\n");
+    }
+    if ($argv[1] <= 0 || $argv[1] >= 50) {
+        exit("Please enter a valid range of workout sets between 1 and 50\n");
+    } elseif ($argv[2] < 30 || $argv[2] > 120) {
+        exit("Please enter a valid workout set time (seconds) between 30 and 120\n");
+    }
 
-if (count($argv) < 3 || !is_int((int) $argv[1]) || !is_int((int) $argv[2])) {
-    exit("Please enter in the following format - generator {number of sets} {set time in seconds}\n");
+    define('TOTALSETS', (int) $argv[1]);
+    define('SETINSECONDS', (int) $argv[2]);
 }
-if ($argv[1] <= 0 || $argv[1] >= 50) {
-    exit("Please enter a valid range of workout sets between 1 and 50\n");
-} elseif ($argv[2] < 30 || $argv[2] > 120) {
-    exit("Please enter a valid workout set time (seconds) between 30 and 120\n");
-}
-
-define('TOTALSETS', (int) $argv[1]);
-define('SETINSECONDS', (int) $argv[2]);
 
 // Generate a workout, from the workout data store, and simply output it
 $workoutDataStore = new WorkoutStore($userData, $exerciseData);
